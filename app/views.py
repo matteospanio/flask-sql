9b708ba9e6582bf33e44d98437b325fdc7522798
Matteo <877485@stud.unive.it>
2021-04-29 22:44:51 +0200

    Changed auth and viws

--- app/views.py
+++ app/views.py
@@ -3,6 +3,9 @@ from flask import Blueprint, render_template
 views = Blueprint('views', __name__)
 
 
-@views.route('/')
+@views.route('/', methods=['GET', 'POST'])
 def home():
-    return render_template("index.html")
+    if method == 'POST':
+        return render_template("home.html", name=request.form.get('name'))
+    else:
+        return render_template("index.html")
